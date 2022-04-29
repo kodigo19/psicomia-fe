d2fab933855603b7211d0866b842b375f9cf1fd9
Fernando Segura <fersegura.sa@gmail.com>
2022-04-29 07:50:30 -0500

    Landing body without format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,41 @@
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       screens: {
         'xs': '480px',
         // => @media (min-width: 480px) { ... }
-
+  
         'sm': '640px',
         // => @media (min-width: 640px) { ... }
-
+  
         'md': '768px',
         // => @media (min-width: 768px) { ... }
-
+  
         'lg': '1024px',
         // => @media (min-width: 1024px) { ... }
-
+  
         'xl': '1280px',
         // => @media (min-width: 1280px) { ... }
-
+  
         '2xl': '1536px',
         // => @media (min-width: 1536px) { ... }
       },
       colors: {
-        'brand-blue': {
-          500: '#1830a3',
-          700: '#0D1958',
+        'brand-purple': {
+          400: '#756e89',
+          500: '#503a89',
+          700: '#503a89',
         },
         'brand-green' : {
-          500: '#34c38f',
-          700: '#1C5F46',
+          500: '#503a89',
+          700: '#503a89',
         },
-        'btn-bg': '#d1bfec',
-        'btn-bc': '#d1bfec',
-        'btn-color': '#41333f',
+        'brand-btn-bg': '#d1bfec',
+        'brand-btn-bc': '#d1bfec',
+        'brand-btn-color': '#41333f',
       }
     },
     fontFamily: {
